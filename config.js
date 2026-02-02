bfe57d1a9bb73adc1ea9edb95c73bc524971cecb
jakzuro11 <jackobgamer15@gmail.com>
2026-02-02 17:28:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/duqaoucpz/video/upload/v1770049552/lobuzy-ft-zenek-martyniuk-mandacik-efektydzwiekowe.com_mxi16h.mp3", // Music streaming URL
         startText: "🎵 Włącz Zenka",        // Button text to start music
         stopText: "🔇 Wyłącz Zenka",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
